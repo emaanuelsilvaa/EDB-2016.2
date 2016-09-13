3313067f7865669b32a9468f5316d527394385e8
Emanuel Borges <emanuelborgesdasilva@gmail.com>
2016-09-13 11:47:51 -0300

    Update Selection Sort.cpp

--- Selection Sort.cpp
+++ Selection Sort.cpp
@@ -17,7 +17,7 @@ struct vetor{
 *
 * @pre v.tam > 0
 * @post para cada elemento v[i] em 'v.elementos', será verdadeiro
-* que v[i] < v[i+1].
+* que v[i] <= v[i+1].
 */
 void selection_sort(vetor v) 
 { 
