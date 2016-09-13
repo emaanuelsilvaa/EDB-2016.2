ac4befbd7227edff2ca149b8629be7be80465554
Emanuel Borges <emanuelborgesdasilva@gmail.com>
2016-09-13 11:05:07 -0300

    Update Merge Sort.cpp

--- Merge Sort.cpp
+++ Merge Sort.cpp
@@ -1,37 +1,89 @@
 #include <iostream>
-#include <iterator>
-#include <algorithm>
-#include <functional>
 
-template <typename BidirIt, typename Compare = std::less<>>
-void merge_sort(BidirIt first, BidirIt last, Compare cmp = Compare {})
-{
-    const auto n = std::distance(first, last);
-    
-    if (n > 1) {
-        const auto middle = std::next(first, n / 2);
-        
-        merge_sort(first, middle, cmp);
-        merge_sort(middle, last, cmp);
-        
-        std::inplace_merge(first, middle, last, cmp);
-    }
+using namespace std;
+//void intercalar(int vetor[], int tamanho, int esquerda, inte direita);
+void merge(int vetor[], int tamanho, int esquerda, int meio, int direita){
+
+	int temp[tamanho];
+	for(int i = esquerda ; i <= direita ; i++){
+
+		temp[i] = vetor[i];
+
+	}
+	int i = esquerda;
+	int j = meio + 1;
+	int k = esquerda;
+	while(i <= meio and j <= direita){
+
+		if(temp[i] <= temp[j]){
+	
+			vetor[k] = temp[i];
+			i++;
+		}
+		else{
+			vetor[k] = temp[j];
+			j++;
+		}
+		k++;
+	}
+	while(i <= meio){
+		temp[k] = temp[i];
+		k++;
+		i++;
+	}
+	while(j <= direita){
+		temp[k] = temp[j];
+		k++;
+		j++;
+	}
 }
 
-#include <vector>
+void intercalar(int vetor[], int tamanho, int esquerda, int direita){
+	
+if(esquerda < direita){
+		int meio = (esquerda + direita)/2;
+		intercalar(vetor, tamanho, esquerda, meio);
+		intercalar(vetor, tamanho, meio+1, direita);
+		merge(vetor, tamanho, esquerda, meio, direita);
+	}
+} 
 
-int main()
-{
-    std::vector<int> v {3, -2, 1, 5, -9, 10, 3, -3, 2};
-    
-	std::cout << "[ ";
-	for(int &e: v)
-		std::cout << e << " "; 
-	std::cout << "]\n";
-    merge_sort(std::begin(v), std::end(v)); // sort increasing
-	std::cout << "[ ";
-	for(int &e: v)
-		std::cout << e << " "; 
-	std::cout << "]\n";
-    merge_sort(std::begin(v), std::end(v), std::greater<> {}); // sort decreasing
+int main (int argc, char** argv){
+
+	srand(time(NULL));
+
+	int n, tam, faixa;	
+	char opcao;
+	
+
+	cout << "digite o tamanho do vetor : " << endl;
+	cin >> tam;
+	int elementos[tam];
+	cout << "Digite a faixa de numeros aleatorios a ser colocados no vetor" << endl;
+	cin >> faixa;
+
+	for(int i{0} ; i < tam; i++){
+		elementos[i] = rand() % faixa;
+		cout << elementos[i] << " ";
+	}
+	cout << endl << "Deseja ordenar o vetor com o algoritmo INSERTION_SORT ?  [S/N]" << endl;
+	cin >> opcao;
+
+	opcao = toupper(opcao);
+	switch(opcao){
+		case 'S':
+			intercalar(elementos,tam,0, tam-1);
+			break;
+		case 'N':
+			return EXIT_SUCCESS;
+			break;
+		default:
+			cout << "Opção invalida" << endl;
+	}
+
+	for(int i = 0 ; i< tam; i++){
+
+		cout << elementos[i]<<" ";
+
+	}	cout << endl;		
 }
